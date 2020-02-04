9fa11edaa5237be8ffbd2d460af34a80b581716e
Danila Luzhin <DLuzhin@nvg.ru>
2020-02-04 17:23:55 +0300

    upd

--- src/blocks/modules/footer/footer.js
+++ src/blocks/modules/footer/footer.js
@@ -2,41 +2,57 @@ import $ from "jquery";
 
 $(document).ready(function () {
 	//stepper
-	var stepper = document.querySelector('.write-modal__stepper');
-	var next = document.querySelector('#step-message');
-	var send = document.querySelector('#step-send');
+	//var stepper = document.querySelector('.modal__stepper');
+	//var next = document.querySelector('.step-message');
+	//var send = document.querySelector('.step-send');
 	var stepperIndex = 1;
 
 	function backToStart() {
 		$('.modal__overlay').removeClass('active');
 		$('body').removeClass('overflow');
-		stepper.style.transform = 'translate(' + (2) * 0 + '%)';
-		$('.write-modal__progress--step:first-child').removeClass('active');
-		$('.write-modal__progress--step:nth-child(2)').removeClass('active');
-		$('.write-modal__progress--finish').removeClass('active');
+		$('.modal__stepper').css({"transform":"translate(0%)"});
+		$('.modal__progress--step:first-child').removeClass('active');
+		$('.modal__progress--step:nth-child(2)').removeClass('active');
+		$('.modal__progress--finish').removeClass('active');
+		$('.modal__bg2, .modal__bg3').hide();
+		$('.modal__bg1').show();
 	}
 
 	$('#modal-contact').on('click', function () {
-		$('.modal__overlay').addClass('active');
+		$('#write-modal').addClass('active');
 		$('body').addClass('overflow');
 	});
-	$('.write-modal__close').on('click', function () {
+
+	$('#modal-ask').on('click', function () {
+		$('#ask-modal').addClass('active');
+		$('body').addClass('overflow');
+	});
+
+	$('.write-modal__close, .ask-modal__close, .modal-404__close').on('click', function () {
 		backToStart();
 	});
 
-	next.addEventListener('click', function(){
+	$('.step-message').on('click', function () {
 		stepperIndex = (stepperIndex < 0) ? stepperIndex - 1 : 0;
-		stepper.style.transform = 'translate(' + (1) * -33.33333 + '%)';
-		$('.write-modal__progress--step:first-child').addClass('active');
+		$('.modal__stepper').css({"transform":"translate(-33.3333%)"});
+		$('.modal__progress--step:first-child').addClass('active');
+		$('.modal__bg1, .modal__bg3').hide();
+		$('.modal__bg2').show();
 	});
-	send.addEventListener('click', function () {
+	$('.step-send').on('click', function () {
 		stepperIndex = (stepperIndex < 1) ? stepperIndex - 1 : 2;
-		stepper.style.transform = 'translate(' + (2) * -33.33333 + '%)';
-		$('.write-modal__progress--step:nth-child(2)').addClass('active');
-		$('.write-modal__progress--finish').addClass('active');
+		$('.modal__stepper').css({"transform":"translate(-66.6666%)"});
+		$('.modal__progress--step:nth-child(2)').addClass('active');
+		$('.modal__progress--finish').addClass('active');
+		$('.modal__bg1, .modal__bg2').hide();
+		$('.modal__bg3').show();
 	});
 
-	$('#step-success').on('click', function () {
+	$('.step-success').on('click', function () {
 		backToStart();
-	})
+	});
+
+	$('.btn__test').on('click',function () {
+		$('.modal__404').addClass('active');
+	});
 });
--- src/blocks/modules/modals/modals.js
+++
@@ -1,8 +0,0 @@
-import $ from "jquery";
-
-$(document).ready(function () {
-	$('#modal-contact').on('click', function () {
-		//$('.modal__overlay').addClass('active');
-		console.log('clicked');
-	});
-});
