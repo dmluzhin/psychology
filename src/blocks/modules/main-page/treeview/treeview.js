370e827de809634df4ee411748545beaa33d8fa2
Данила Лужин <dluzhin@nvg.ru>
2020-03-02 18:54:28 +0300

    upd

--- src/blocks/modules/main-page/treeview/treeview.js
+++ src/blocks/modules/main-page/treeview/treeview.js
@@ -11,4 +11,16 @@ $(document).ready(function () {
 		$('#content').find('.treeview__group').hide();
 		$(content).fadeIn(200);
 	});
+
+	var tabsHidden = $(".treeview__scheme .treeview__scheme--scroller-hider .treeview__scheme--scroller .treeview__scheme--scroller-list .treeview__btn");
+
+	tabsHidden.on('click', function (e) {
+		console.log('tab clicked');
+		e.preventDefault();
+		var content = this.hash.replace('/', '');
+		tabsHidden.removeClass('treeview__btn--active');
+		$(this).addClass('treeview__btn--active');
+		$('#content').find('.treeview__group').hide();
+		$(content).fadeIn(200);
+	});
 });
